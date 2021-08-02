be3c01f1e249707ee0369a035b1888394a98a2cc
pashutk <me@pashutk.ru>
2021-08-03 01:43:47 +0300

    Release

---
+++ docs/_snowpack/pkg/@heroicons/react/solid.js
@@ -0,0 +1,18 @@
+import { r as react } from '../../common/index-04edb6a1.js';
+import '../../common/_commonjsHelpers-8c19dec8.js';
+
+function SaveIcon(props) {
+  return /*#__PURE__*/react.createElement("svg", Object.assign({
+    xmlns: "http://www.w3.org/2000/svg",
+    viewBox: "0 0 20 20",
+    fill: "currentColor"
+  }, props), /*#__PURE__*/react.createElement("path", {
+    d: "M7.707 10.293a1 1 0 10-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 11.586V6h5a2 2 0 012 2v7a2 2 0 01-2 2H4a2 2 0 01-2-2V8a2 2 0 012-2h5v5.586l-1.293-1.293zM9 4a1 1 0 012 0v2H9V4z"
+  }));
+}
+
+var SaveIcon_1 = SaveIcon;
+
+var SaveIcon$1 = SaveIcon_1;
+
+export { SaveIcon$1 as SaveIcon };
