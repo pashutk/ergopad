3db273eff0d89c8a1231ebb296c3dedd7531c4bc
pashutk <me@pashutk.ru>
2026-04-01 00:38:35 +0200

    Release

--- docs/_snowpack/pkg/fp-ts/lib/function.js
+++
@@ -1,8 +0,0 @@
-import { _ as _function } from '../../common/function-eb01d8e0.js';
-import '../../common/_commonjsHelpers-8c19dec8.js';
-
-
-
-var absurd = _function.absurd;
-var pipe = _function.pipe;
-export { absurd, pipe };
--- docs/_snowpack/pkg/react-dom.js
+++
@@ -1,4 +0,0 @@
-import { r as reactDom } from './common/index-3eae4d6e.js';
-export { r as default } from './common/index-3eae4d6e.js';
-import './common/_commonjsHelpers-8c19dec8.js';
-import './common/index-04edb6a1.js';
